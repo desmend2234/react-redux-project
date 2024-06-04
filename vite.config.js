53e200f29cfb39c5d20839dafb0b8e55239c99b3
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2024-06-04 22:02:14 +0800

    增加base後重新npm run build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/react-redux-project/',
     plugins: [react()],
     data() {
         return {
