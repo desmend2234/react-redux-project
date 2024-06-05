83ed7c352fcac7d63ef6ec9985811bacc2ed34bb
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2024-06-05 09:20:19 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    publicPath: 'react-redux-project',
     plugins: [react()],
     data() {
         return {
